08429bcb3ad9f6141ac90fb428c8b8e462fb70aa
Naftal Timotheus <naftal@efinity.com.na>
2016-11-13 11:39:37 +0200

    FAST COMMIT, SAVEPOINT

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,27 @@
 
 @section('content')
 <div class="container">
+    <div class="jumbotron">
+        <h1>IOB - Portal</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+    </div>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+        <div class="col-lg-3 col-md-3 col-sm-4">
+            <div class="list-group table-of-contents">
+                <a class="list-group-item" href="#navbar">Profile</a>
+                <a class="list-group-item" href="#buttons">Courses</a>
+                <a class="list-group-item" href="#typography">Accademic Records</a>
+                <a class="list-group-item" href="#tables">Attendance</a>
+                <a class="list-group-item" href="#tables">
+                    <hr></a>
+                <a class="list-group-item" href="#forms">Admin Panel</a>
+                <a class="list-group-item" href="#navs">Stuffs Panel</a>
+
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="panel panel-primary">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,9 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li class="active"><a href="{{ url('home') }}">Home</a></li>
+                        <li class=""><a href="{{ url('home') }}">Courses</a></li>
+                        <li class=""><a href="{{ url('home') }}">Academics</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
